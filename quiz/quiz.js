229f5b77f31d4350ae00b27805fa28b8d7430dad
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2018-10-10 14:05:07 +0100

    Add files via upload

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,50 +1,75 @@
-var questionOne = prompt('Question one: What is the capital of Japan?');
-var question1 = questionOne.toUpperCase();
-var score = 0
- if(question1 === 'TOKYO') {
-   alert('Thats right!')
-   score++;
- } else {
-   alert('Sorry thats wrong')
- }
-  console.log(score)
+var questions = [
+  ["Halloween is also known as All Hallows Eve, what is another name for Hallows?", "saints"],
+  ["Which game involving fruit is commonly played at Halloween?", "bobbing for apples"],
+  ["Who are Dr. Peter Venkman, Dr. Raymond Stantz, and Dr. Egon Spengler better known as?", "ghostbusters"],
+  ["What is the name for a ghost responsible for physical disturbances, such as loud noises and objects being moved?", "poltergeist"],
+  ["Which TV series had the characters Fred, Velma, and Daphne?", "scooby doo"]
+];
 
- var questionTwo = prompt('Question two: How many continents are there?');
- var question2 = questionTwo.toUpperCase();
-  if(question2 === '7' || question2 === 'SEVEN') {
-    alert('That is correct!')
-    score++;
-  } else {
-    alert('Thats wrong!');
-  }
-   console.log(question2);
+var question;
+var answers;
+var score = 0;
+var correct = [];
+var wrong = [];
 
-  var questionThree = prompt('Question three: What is the Turkish word for lamb?');
-  var question3 = questionThree.toUpperCase();
-   if (question3 === 'KEBAB') {
-     alert('Correcto!')
-     score++;
-   } else {
-     alert('Wrong im afraid..')
+function print(message) {
+  var outputDiv = document.getElementById('output')
+  outputDiv.innerHTML = message;
+}
+
+function listBuilder(array) {
+  var list = '<ol>';
+   for (var i = 0; i < array.length; i+= 1) {
+     list += '<li>' + array[i] + '</li>';
    }
-    console.log(score)
+   list += '</ol>';
+   return list;
+}
 
-    var questionFour = prompt('Question four: Which plant does the Canadian flag contain?');
-    var question4 = questionFour.toUpperCase();
-     if (question4 === 'MAPLE') {
-       alert('You got it!')
-       score++;
-     } else {
-       alert('No thats wrong!');
-     }
 
-     var questionFive = prompt('Final Question: In which Nintendo DS game do you have to raise a puppy as well as possible?');
-     var question5 = questionFive.toUpperCase();
-      if (question5 === 'NINTENDOGS') {
-        alert('Perfect!')
-        score++;
+for (var i = 0; i < questions.length; i += 1) {
+  question = prompt(questions[i][0])
+  answers = questions[i][1]
+  response = question.toLowerCase();
+  console.log(response);
+  if (response === answers) {
+  score += 1
+  correct.push(questions[i][0]);
+  console.log(score)
+  console.log(correct);
+} else {
+  wrong.push(questions[i][0])
+}
+}
+
+html = '<h1>Congratulations you got ' + score + ' correct!</h1>'
+html += '<h2>You got these questions correct:</h2>';
+html += listBuilder(correct);
+html += '<h2>You got these questions wrong:</h2>';
+html += listBuilder(wrong);
+if (score < 1) {
+  html += '<p>No crown for you but heres a puppy to cheer you up!</p>'
+  html += '<img src ="dog.jpg" width="310" height="220">';
+} else {
+  if (score <= 2) {
+    html += '<p>You did okay.. Take this apple!</p>';
+    html += '<img src ="apple.jpg" width="310" height="320">';
+  } else {
+    if (score <= 3) {
+      html += '<p>Not bad! Here is a toothbrush!</p>';
+      html += '<img src ="toothbrush.jpg" width="310" height="320">';
+    } else {
+      if (score <= 4) {
+        html += '<p>You did great! Take a cookie but only one.</p>';
+        html += '<img src ="cookies.jpg" width="310" height="320">';
       } else {
-        alert('Oh no.');
+        if (score === 5) {
+          html += '<p>You got them all right and unlocked the motherload! Congratulations!</p>';
+          html += '<img src ="sweets.jpg" width="310" height="320">';
+        }
       }
+    }
+  }
+}
 
-      document.write('<p>Congratulations you finished the quiz and got ' + score + ' correct!</p>');
+print(html)
