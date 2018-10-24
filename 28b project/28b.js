b99e1b5d26c4454e7bed3b6c5facc6a3a52e3861
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2018-10-24 18:34:16 +0100

    Add files via upload

--- 28b project/28b.js
+++ 28b project/28b.js
@@ -25,14 +25,61 @@ Donut();
 
 //// TODO: Create a for loop that puts out 6 numbers between 0 and 100 and add them together.
 //If it does not equal 100 repeat the loop//
-var percentagesArray = []
+var numbersArray = []
 var maximum = 6;
-for (var i = 0; i < maximum; i++) {
-  var adding = getRandomNumber(100)
-  percentagesArray.push(adding)
-  var reducer = (accumulator, currentValue) => accumulator + currentValue;
+var percentArray = []
+
+
+function getPercent() {
+  for (var i = 0; i < maximum; i++) {
+    var addingToArray = getRandomNumber(100)
+    numbersArray.push(addingToArray)
+  }
+   var sum = numbersArray.reduce((total, amount) => total + amount);
+    for (var i = 0; i < numbersArray.length; i++) {
+      var percent = Math.floor(numbersArray[i]*100/sum);
+      percentArray.push(percent)
+    }
+  }
+
+function widths() {
+  document.getElementById('avgOne').style.width = numbersArray[0] + "%";
+  document.getElementById('avgTwo').style.width = numbersArray[1] + "%";
+  document.getElementById('avgThree').style.width = numbersArray[2] + "%";
+  document.getElementById('avgFour').style.width = numbersArray[3] + "%";
+  document.getElementById('avgFive').style.width = numbersArray[4] + "%";
+  document.getElementById('avgSix').style.width = numbersArray[5] + "%";
+
+  document.getElementById('cautiousOne').style.width = numbersArray[5] + "%";
+  document.getElementById('cautiousTwo').style.width = numbersArray[3] + "%";
+  document.getElementById('cautiousThree').style.width = numbersArray[2] + "%";
+  document.getElementById('cautiousFour').style.width = numbersArray[4] + "%";
+  document.getElementById('cautiousFive').style.width = numbersArray[6] + "%";
+  document.getElementById('cautiousSix').style.width = numbersArray[1] + "%";
+
+  document.getElementById('complacentOne').style.width = numbersArray[4] + "%";
+  document.getElementById('complacentTwo').style.width = numbersArray[1] + "%";
+  document.getElementById('complacentThree').style.width = numbersArray[6] + "%";
+  document.getElementById('complacentFour').style.width = numbersArray[2] + "%";
+  document.getElementById('complacentFive').style.width = numbersArray[3] + "%";
+  document.getElementById('complacentSix').style.width = numbersArray[5] + "%";
+
+  document.getElementById('confidentOne').style.width = numbersArray[2] + "%";
+  document.getElementById('confidentTwo').style.width = numbersArray[6] + "%";
+  document.getElementById('confidentThree').style.width = numbersArray[1] + "%";
+  document.getElementById('confidentFour').style.width = numbersArray[5] + "%";
+  document.getElementById('confidentFive').style.width = numbersArray[4] + "%";
+  document.getElementById('confidentSix').style.width = numbersArray[3] + "%";
 }
 
-console.log(percentagesArray.reduce(reducer))
+getPercent();
+widths();
+
+
+
+console.log(percentArray)
+console.log(numbersArray)
+
+
 
 //// TODO: Create an array of objects and name and a randomizer to output the data on the overview div.
