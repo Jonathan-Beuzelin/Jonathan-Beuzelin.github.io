964d32ab68181eb492e60fd792bc4fc82ac63bf5
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2018-10-28 11:28:53 +0000

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -9,6 +9,17 @@ document.querySelectorAll('a[href^="#hiddenanchor"]').forEach(anchor => {
     });
 });
 
+document.querySelectorAll('a[href^="#hiddenanchor1"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
 document.querySelectorAll('a[href^="#hiddenanchor2"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
