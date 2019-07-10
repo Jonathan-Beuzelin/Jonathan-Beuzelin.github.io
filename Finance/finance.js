b848f64fbc6f9c79fa28fdc10ae923660b3c22ff
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2019-07-10 21:39:39 +0100

    SVG Pie chart & Currency.
    Currency update so that when the currency is changed in the menu it will change all the currency displayed on the page.
    SVG pie chart added with colour coded list elements.
    Pie chart colours and style will be updated in the future.

--- Finance/finance.js
+++ Finance/finance.js
@@ -5,11 +5,52 @@ const columnTwo = document.getElementById("columntwo");
 let one = document.getElementById("expenditure").elements;
 let total = 0;
 let arrayOfExpenditure = [];
-const currency = document.getElementById('currency')
+const currency = document.getElementById("currency");
+
+let data = [
+  {
+   "name": "food",
+   "color": "red",
+    "value": 0
+  }, {
+   "name": "bills",
+   "color": "rebeccapurple",
+    "value": 0
+  }, {
+   "name": "transport",
+   "color": "green",
+    "value": 0
+  }, {
+   "name": "clothing",
+   "color": "pink",
+    "value": 0
+  }, {
+   "name": "social",
+   "color": "blue",
+    "value": 0
+  }, {
+   "name": "health",
+   "color": "yellow",
+    "value": 0
+  }, {
+   "name": "other",
+   "color": "grey",
+    "value": 0
+  }
+];
+
+setInterval(function() {
+  let currencyIcon = document.querySelectorAll(".unit");
+  let currencySelected = currency.value;
+  for (let i = 0; i < currencyIcon.length; i++) {
+    currencyIcon[i].innerHTML = currencySelected
+  }
+}, 10)
 
 addNewRow.addEventListener("click", function() {
-  columnOne.insertAdjacentHTML("beforeend","<input type='text' name='expenditure' value='' placeholder='Amount' id='expenditure' class='expenditure'>");
-  columnTwo.insertAdjacentHTML("beforeend","<select id='pet-select''><option value=''>--Please choose an option--</option><option value='dog'>Dog</option><option value='cat'>Cat</option><option value='hamster'>Hamster</option><option value='parrot'>Parrot</option><option value='spider'>Spider</option><option value='goldfish'>Goldfish</option></select>");
+  let currencySelected = currency.value;
+  columnOne.insertAdjacentHTML("beforeend",'<div class="row"><span class="unit">'+ currencySelected +'</span><input type="text" name="expenditure" value=""  placeholder="Amount" id="expenditure" class="expenditure"></div>');
+  columnTwo.insertAdjacentHTML("beforeend",'<select id="expendituretype"><option value="">--Please choose an option--</option><option value="food">Food</option><option value="bills">Bills</option><option value="transport">Transport</option><option value="clothing">Clothing</option><option value="social">Social</option><option value="health">Health</option><option value="other">Other</option>elect>');
 });
 
 setInterval(function(){
@@ -23,12 +64,13 @@ setInterval(function(){
       arrayOfExpenditure.push(turnToFloat)
     }
   }
+
   if (arrayOfExpenditure.length != 0) {
     let sum = arrayOfExpenditure.reduce((total, amount) => total + amount);
     let monthly = parseFloat(document.getElementById("monthly").value)
     let total = monthly - sum
     let total2 = total.toFixed(2)
-    currencySelected = currency.options[currency.selectedIndex].text;
+    let currencySelected = currency.options[currency.selectedIndex].value;
     let sentence = "<p>Your total savings every month are " + currencySelected + total2 + "</p>"
     totalDisplay.innerHTML = sentence
     arrayOfExpenditure.length = 0;
@@ -36,4 +78,61 @@ setInterval(function(){
     let sentence = "<p>Please insert a number</p>"
     totalDisplay.innerHTML = sentence
   }
-}, 10);
+
+  const expenditureType = document.querySelectorAll("#expendituretype")
+  for (let i = 0; i < expenditureType.length; i++) {
+    for (let x = 0; x < data.length; x++) {
+      if (expenditureType[i].value === data[x].name) {
+        data[x].value++
+      }
+    }
+  }
+
+  totalValue = 0,
+  radius = 60,
+  circleLength = Math.PI * (radius * 2)
+  spaceLeft = circleLength;
+
+  for (let i = 0; i < data.length; i++) {
+    totalValue += data[i].value;
+  }
+
+  circle = document.querySelectorAll('circle')
+
+  for (let c = 0; c < data.length; c++) {
+    circle[c].setAttribute("class", "pie-chart-value");
+    circle[c].setAttribute("cx", 150);
+    circle[c].setAttribute("cy", 150);
+    circle[c].setAttribute("r", radius);
+
+    // Set dash on circle
+    circle[c].style.strokeDasharray = (spaceLeft) + " " + circleLength;
+
+    // Set Stroke color
+    circle[c].style.stroke = data[c].color;
+
+    // Subtract current value from spaceLeft
+    spaceLeft -= (data[c].value / totalValue) * circleLength;
+
+    // Add value to list.
+    let listItem = document.getElementById('pie-values'),
+        valuePct = parseFloat((data[c].value / totalValue) * 100).toFixed(1);
+
+    // Add text to list item
+
+    list = document.querySelectorAll('li')
+
+    if (isNaN(valuePct) ) {
+      list[c].innerHTML = data[c].name + ' (' + 0 + '%)'
+    } else {
+      list[c].innerHTML = data[c].name + ' (' + valuePct + '%)'
+    }
+
+    // Set color of value to create relation to pie.
+    list[c].style.color = data[c].color;
+  }
+
+  for (let a = 0; a < data.length; a++) {
+    data[a].value = 0;
+  }
+}, 1000);
