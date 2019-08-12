f70ec80b31d1c62b52eb8e7ce219fd42d09d239c
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2019-08-12 01:57:37 +0100

    Title added & JavaScript linted

--- Translation/index.js
+++ Translation/index.js
@@ -1,9 +1,9 @@
 function getText() {
-  const books = document.getElementsByClassName('book_selection')
+  const books = document.getElementsByClassName('book_selection');
   for (var i = 0; i < books.length; i++) {
     books[i].addEventListener("click", function(e){
-      e.preventDefault()
-      document.getElementById('title').innerHTML = e.target.innerHTML
+      e.preventDefault();
+      document.getElementById('title').innerHTML = e.target.innerHTML;
       fetch(e.target.innerHTML.replace(/[^a-zA-Z]/g, "")+".txt")
       .then(function(response){
         return response.text();
@@ -14,9 +14,9 @@ function getText() {
       })
       .then(function(){
         $(".word_split").lettering('words');
-        getTranslation()
-      })
-    })
+        getTranslation();
+      });
+    });
   }
 }
 
@@ -24,9 +24,9 @@ function getTranslation() {
   const word = document.getElementsByTagName('span');
   for (let i = 0; i < word.length; i++) {
     word[i].addEventListener("click", function(e) {
-      document.querySelector('#translationOutput').innerHTML = " "
-      document.getElementById('dictionaryOutput').innerHTML = " "
-      document.getElementById('pos').innerHTML = " "
+      document.querySelector('#translationOutput').innerHTML = " ";
+      document.getElementById('dictionaryOutput').innerHTML = " ";
+      document.getElementById('pos').innerHTML = " ";
       e.target.style.fontWeight = 600;
       let api_key = 'trnsl.1.1.20190803T205532Z.8647ed72c2fd9935.88fa851b8a0d3b85ed70dc1ceb33a675c477242b';
       fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + api_key + '&text=' + e.target.innerHTML + '&lang=fr-en&format=html')
@@ -35,7 +35,7 @@ function getTranslation() {
       })
       .then(res => {
         for (var i = 0; i < res.text.length; i++) {
-          document.querySelector('#translationOutput').innerHTML = res.text[i].replace(/[^a-zA-Z]/g, " ")
+          document.querySelector('#translationOutput').innerHTML = res.text[i].replace(/[^a-zA-Z]/g, " ");
         }
       });
       let api_dictionary = 'dict.1.1.20190805T202709Z.335da31d890b6512.d631a95d13606b8a5c82e663ad3f7f2760869ba1';
@@ -46,22 +46,22 @@ function getTranslation() {
       .then(res => {
         if (res.def[0]) {
           for (let i = 0; i < res.def[0].tr.length; i++) {
-            document.getElementById('dictionaryOutput').innerHTML += '<p>' + res.def[0].tr[i].text.replace(/[^a-zA-Zá]/g, " ") +  ' <br></p>'
-            document.getElementById('pos').innerHTML += '<p>' + res.def[0].tr[i].pos.replace(/[^a-zA-Z]/g, " ") + ' <br></p>'
+            document.getElementById('dictionaryOutput').innerHTML += '<p>' + res.def[0].tr[i].text.replace(/[^a-zA-Zá]/g, " ") +  ' <br></p>';
+            document.getElementById('pos').innerHTML += '<p>' + res.def[0].tr[i].pos.replace(/[^a-zA-Z]/g, " ") + ' <br></p>';
             console.log(res);
           }
         } else {
-          document.getElementById('dictionaryOutput').innerHTML = "<p>None Found</p>"
+          document.getElementById('dictionaryOutput').innerHTML = "<p>None Found</p>";
         }
-      })
-    })
+      });
+    });
     word[i].addEventListener("mouseout", function(e) {
       e.target.style.fontWeight = "";
-      document.querySelector('#translationOutput').innerHTML = " "
-      document.getElementById('dictionaryOutput').innerHTML = " "
-      document.getElementById('pos').innerHTML = " "
+      document.querySelector('#translationOutput').innerHTML = " ";
+      document.getElementById('dictionaryOutput').innerHTML = " ";
+      document.getElementById('pos').innerHTML = " ";
     });
-  };
+  }
 }
 
 
