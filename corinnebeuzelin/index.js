854d6da52d93df260c6530da1573475aa9ff1b9d
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2020-01-27 22:44:01 +0000

    Added functions and mobile css
    Added webkit for mobile to stop checkbox being visible and function to hide the menu when scrolling down and reveal when scrolling up.
    Menu moved to the top of the webpage as it conflicted with safari native navigation.

--- corinnebeuzelin/index.js
+++ corinnebeuzelin/index.js
@@ -2,41 +2,40 @@ const menu = document.getElementById('menu')
 const checkbox = document.querySelector("input[name=checkbox]");
 
 window.addEventListener('scroll', function() {
- fadeIn(document.querySelector('.servicebutton'))
- fadeIn(document.querySelector('form'))
+  fadeIn(document.querySelector('.servicebutton'))
+  fadeIn(document.querySelector('form'))
 });
 
 
 function fadeIn(fadeInSubject) {
-	const position = fadeInSubject.getBoundingClientRect();
-	// checking whether fully visible
-	if(position.top >= 0 && position.bottom <= window.innerHeight) {
-		if (fadeInSubject.style.opacity != "100%") {
-			fadeInSubject.style.opacity = "100%"
-			}
-			if (fadeInSubject.style.marginTop = "0") {
-				fadeInSubject.style.marginTop = "20px"
-		};
-	};
+  const position = fadeInSubject.getBoundingClientRect();
+  // checking whether fully visible
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    if (fadeInSubject.style.opacity != "100%") {
+      fadeInSubject.style.opacity = "100%"
+    }
+    if (fadeInSubject.style.marginTop = "0") {
+      fadeInSubject.style.marginTop = "20px"
+    };
+  };
 };
 
 
-checkbox.addEventListener( 'change', function() {
-    if(this.checked) {
-      menu.style.top = "-205px"
-    } else {
-      menu.style.top = "70px"
-    }
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    menu.style.opacity = "100%"
+  } else {
+    menu.style.opacity = "0%"
+  }
 });
-
 
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos || menu.style.top == "-205px") {
-    document.getElementById('nav').style.bottom = "0";
+    document.getElementById('nav').style.top = "0";
   } else {
-    document.getElementById('nav').style.bottom = "-70px";
+    document.getElementById('nav').style.top = "-70px";
   }
   prevScrollpos = currentScrollPos;
 }
