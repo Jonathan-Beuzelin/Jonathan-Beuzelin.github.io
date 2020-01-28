a6d6bb50f0889de63f7ff72c7098490f29424543
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2020-01-28 16:54:36 +0000

    Removed navigation scroll interaction.
    Added visibility collapse on checkbox on checked.

---
+++ index.js
@@ -0,0 +1,43 @@
+const menu = document.getElementById('menu')
+const checkbox = document.querySelector("input[name=checkbox]");
+
+window.addEventListener('scroll', function() {
+  fadeIn(document.querySelector('.servicebutton'))
+  fadeIn(document.querySelector('form'))
+});
+
+
+function fadeIn(fadeInSubject) {
+  const position = fadeInSubject.getBoundingClientRect();
+  // checking whether fully visible
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    if (fadeInSubject.style.opacity != "100%") {
+      fadeInSubject.style.opacity = "100%"
+    }
+    if (fadeInSubject.style.marginTop = "0") {
+      fadeInSubject.style.marginTop = "20px"
+    };
+  };
+};
+
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    menu.style.opacity = "100%"
+    menu.style.visibility = "visible"
+  } else {
+    menu.style.visibility = "collapse"
+    menu.style.opacity = "0%"
+  }
+});
+
+/*let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos || menu.style.top == "-205px") {
+    document.getElementById('nav').style.top = "0";
+  } else {
+    document.getElementById('nav').style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}*/
