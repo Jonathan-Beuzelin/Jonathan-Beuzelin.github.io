dd865732e16b2996685f02ca19de76f6a354ef59
Jonathan Beuzelin <jonathan.beuzelin@gmail.com>
2022-10-16 23:01:45 +0100

    ForEach used instead of for loops
    ForEach used instead of for loops to apply colours and now shows seed colour. Event listeners no longer stack when button is clicked.

--- ColorGenerator/index.js
+++ ColorGenerator/index.js
@@ -1,28 +1,33 @@
 const colorPicked = document.getElementById("colorPicker");
 const mode = document.getElementById("mode");
 const getScheme = document.getElementById("getColours");
-const main = document.getElementById("main");
-let coloursArray = [];
-let hexCodeArray = [];
+const colorDivs = document.querySelectorAll(".color");
+const hexDivs = document.querySelectorAll(".hexcode");
+
 getScheme.addEventListener("click", function (e) {
   let modePicked = mode.value;
   let hexCode = colorPicked.value.slice(1);
   e.preventDefault;
   fetch(
-    `https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${modePicked}&count=5`
+    `https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${modePicked}&count=4`
   )
     .then((res) => res.json())
     .then((data) => {
-      for (let i = 0; i < data.colors.length; i++) {
-        coloursArray[i] = document.getElementById("color" + [i]);
-        coloursArray[i].style.background = data.colors[i].hex.value;
-        hexCodeArray[i] = document.getElementById("hexCode" + [i]);
-        hexCodeArray[i].innerHTML = data.colors[i].hex.value;
-        coloursArray[i].addEventListener("click", function () {
-          navigator.clipboard.writeText(hexCodeArray[i].textContent)
-          coloursArray[i].innerHTML = '<div class="copied">copied</div>'
-          setTimeout(() => coloursArray[i].innerHTML = "", 2000)
-        });
-      }
+      colorDivs[colorDivs.length - 1].style.backgroundColor =
+        data.seed.hex.value;
+      hexDivs[hexDivs.length - 1].innerHTML = data.seed.hex.value;
+      data.colors.forEach((element, index) => {
+        colorDivs[index].style.backgroundColor = element.hex.value;
+        hexDivs[index].innerHTML = element.hex.value;
+      });
     });
+});
+
+
+colorDivs.forEach((element, index) => {
+  element.addEventListener("click", function () {
+    navigator.clipboard.writeText(hexDivs[index].textContent);
+    element.innerHTML = "<p class='copied'>copied!</p>";
+    setTimeout(() => (element.innerHTML = ""), 1000);
+  });
 });
